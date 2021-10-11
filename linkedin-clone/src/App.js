6d3fdee61a9fd784752a2c1b77ca0fdc303190a0
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-11 11:48:12 +0200

    abnrt

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,8 +1,14 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import './assats/css/profile_page css/Navbar.css';
+import Navbar from './components/profile_page/Navbar';
 
 function App() {
-  return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Navbar />
+		</div>
+	);
 }
 
 export default App;
--- linkedin-clone/src/components/profile_page/Navbar.jsx
+++ linkedin-clone/src/components/profile_page/Navbar.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Navbar() {
-	return <div></div>;
+	return (
+		<div className="header">
+			<div className="header_left">
+				<img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" />
+				<div className="header_search">
+					<img src="SearchIcon" alt="" />
+					<input type="text" name="" placeholder="Search" id="" />
+				</div>
+			</div>
+			<div className="header_right"></div>
+		</div>
+	);
 }
