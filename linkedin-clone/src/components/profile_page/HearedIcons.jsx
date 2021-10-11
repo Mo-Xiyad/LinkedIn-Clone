2627707b5bcd913ef2a91e2aa3f89e639e35f8f1
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-11 14:48:02 +0200

    update

--- linkedin-clone/src/components/profile_page/HearedIcons.jsx
+++ linkedin-clone/src/components/profile_page/HearedIcons.jsx
@@ -3,7 +3,7 @@ import HomeIcon from '@material-ui/icons/Home';
 export default function HearedIcons({ title, icon }) {
 	return (
 		<div className="hearedicons">
-			{icon && <img className="heared_icons" width="20px" src={icon} />}
+			{icon && <img className="heared_icons" width="40px" src={icon} />}
 			<h5 className="heared_title">{title}</h5>
 		</div>
 	);
