6ccf9078ae692458a56ac96836ea299599544e8a
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-11 20:41:12 +0200

    update

--- linkedin-clone/src/components/profile_page/HeaderIcons.jsx
+++ linkedin-clone/src/components/profile_page/HeaderIcons.jsx
@@ -5,7 +5,7 @@ export default function HeaderIcons({ avatar, title, Icon }) {
 		<div className="hearedicons">
 			{Icon && <Icon className="heared_icons" src={Icon} />}
 			<h5 className="heared_title">{title}</h5>
-			{avatar && <Avatar className="heared_icons" src={avatar} />}
+			{avatar && <Avatar className="heared_avatar " src={avatar} />}
 		</div>
 	);
 }
--- linkedin-clone/src/components/profile_page/Navbar.jsx
+++ linkedin-clone/src/components/profile_page/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import HeaderIcons from './HearedIcons';
+import HeaderIcons from './HeaderIcons';
 import SearchIcon from '@mui/icons-material/Search';
 import HomeIcon from '@mui/icons-material/Home';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
@@ -7,13 +7,14 @@ import SmsIcon from '@mui/icons-material/Sms';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import AppsIcon from '@mui/icons-material/Apps';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 export default function Navbar() {
 	return (
 		<div className="header">
 			<div className="header_left">
 				<img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" />
 				<div className="header_search">
-					<SearchIcon />
+					<SearchIcon className="header_search_icon" />
 					<input type="text" name="" placeholder="Search" id="" />
 				</div>
 			</div>
@@ -22,13 +23,17 @@ export default function Navbar() {
 				<HeaderIcons title="Home" Icon={HomeIcon} />
 				<HeaderIcons title="My Network" Icon={PeopleAltIcon} />
 				<HeaderIcons title="Jobs" Icon={BusinessCenterIcon} />
-				<HeaderIcons title="Jobs" Icon={SmsIcon} />
-				<HeaderIcons title="Messaging" Icon={NotificationsIcon} />
-				<HeaderIcons title="Messaging" Icon={AppsIcon} />
+				<HeaderIcons title="Messaging" Icon={SmsIcon} />
+				<HeaderIcons title="Notifications" Icon={NotificationsIcon} />
 
 				<HeaderIcons
 					avatar="https://www.g20.org/wp-content/uploads/2021/01/people.jpg"
 					title="ME"
+					Icon={ArrowDropDownIcon}
+				/>
+				<HeaderIcons
+					title="Work"
+					Icon={ArrowDropDownIcon}
 				/>
 			</div>
 		</div>
