363f47c78ee51a382933afb5419ebccecde612d4
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-12 01:50:46 +0200

    homepage

--- linkedin-clone/src/components/home_page/Sidebar.jsx
+++ linkedin-clone/src/components/home_page/Sidebar.jsx
@@ -5,15 +5,23 @@ export default function Sidebar() {
 	return (
 		<div className="sidebar">
 			<div className="sidebar_avatar">
-				<img />
+				<img
+					className="img-fluid"
+					src="https://png.pngtree.com/background/20210714/original/pngtree-blue-carbon-background-with-sport-style-and-golden-light-picture-image_1200848.jpg"
+				/>
 				<Avatar />
 				<h3>Abnet Nega</h3>
 				<h5>React Developer| Mobile App | Web Page | UXUI </h5>
 			</div>
+
 			<div className="sidebar_texts">
 				<div className="textclass_1">
 					<h5 className="text">Connections</h5>
 					<h6 className="number">121</h6>
+				</div>
+				<div className="textclass_1">
+					<h5 className="text">Who viewed your profile</h5>
+					<h6 className="number">21</h6>
 				</div>
 
 				<div className="textclass_2"></div>
