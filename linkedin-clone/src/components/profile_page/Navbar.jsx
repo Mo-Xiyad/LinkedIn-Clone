8cf1bfb12968ebb954afdb871538c0160a9e0d37
tetiana <tetianayaremko@gmail.com>
2021-10-13 15:14:22 +0300

    hides nav

--- linkedin-clone/src/components/profile_page/Navbar.jsx
+++ linkedin-clone/src/components/profile_page/Navbar.jsx
@@ -7,6 +7,7 @@ import SmsIcon from "@mui/icons-material/Sms";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import AppsIcon from "@mui/icons-material/Apps";
+import Hidden from "@mui/material/Hidden";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import { withRouter } from "react-router";
 import Melogo from "./Melogo";
@@ -27,23 +28,25 @@ function Navbar({ authorized }) {
         </div>
       </div>
 
-      <div className="header_right">
-        <HeaderIcons to={"/"} title="Home" Icon={HomeIcon} />
-        <HeaderIcons title="My Network" Icon={PeopleAltIcon} />
-        <HeaderIcons title="Jobs" Icon={BusinessCenterIcon} />
-        <HeaderIcons title="Messaging" Icon={SmsIcon} />
-        <HeaderIcons title="Notifications" Icon={NotificationsIcon} />
+      <Hidden lgDown>
+        <div className="header_right">
+          <HeaderIcons to={"/"} title="Home" Icon={HomeIcon} />
+          <HeaderIcons title="My Network" Icon={PeopleAltIcon} />
+          <HeaderIcons title="Jobs" Icon={BusinessCenterIcon} />
+          <HeaderIcons title="Messaging" Icon={SmsIcon} />
+          <HeaderIcons title="Notifications" Icon={NotificationsIcon} />
 
-        <Link to={"/profile/me"}>
-          <Melogo title="ME" authorized={authorized} />
-        </Link>
+          <Link to={"/profile/me"}>
+            <Melogo title="ME" authorized={authorized} />
+          </Link>
 
-        <HeaderIcons title="Work" Icon={AppsIcon} />
-        <div className="notif_text">
-          <p className="mb-0">Try Preminum for </p>
-          <p className="mb-0">Free</p>
+          <HeaderIcons title="Work" Icon={AppsIcon} />
+          <div className="notif_text">
+            <p className="mb-0">Try Preminum for </p>
+            <p className="mb-0">Free</p>
+          </div>
         </div>
-      </div>
+      </Hidden>
     </div>
   );
 }
