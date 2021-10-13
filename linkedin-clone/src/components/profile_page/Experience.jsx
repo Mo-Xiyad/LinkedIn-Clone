dc5ffff01b83d7b0a785aa820f3f25b7894c2436
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-13 12:38:41 +0200

    started

--- linkedin-clone/src/components/profile_page/Experience.jsx
+++ linkedin-clone/src/components/profile_page/Experience.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 export default function Experience() {
 	return (
 		<div className="experience">
-			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita id
-			sequi nisi repellendus voluptate quo porro cumque nobis sunt eos
-			consequatur labore, deserunt dolore tenetur vel impedit. Pariatur, ducimus
-			doloremque!
+			<h5 className="experience_title">Experience</h5>
+			<div className="experience_list">
+				<img
+					className="img-fluid wiges_onlibottom_listimg"
+					src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
+				/>
+				<div className="activity_onlybottom_list_txt">
+					<h5>Lorem, ipsum dolor onsectetur</h5>
+					<p>User Comented</p>
+				</div>
+			</div>
 		</div>
 	);
 }
