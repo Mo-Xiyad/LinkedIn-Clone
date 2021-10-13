979df2578fe0cdda476ac69a477783949c924cbf
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-13 02:56:37 +0200

    wid done

---
+++ linkedin-clone/src/components/home_page/Widget_ad.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Widget_ad() {
+    return (
+			<div className="wiges_bottom_list">
+				<img
+					className="img-fluid wiges_onlibottom_listimg"
+					src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
+				/>
+
+				<div className="wiges_onlybottom_list_txt">
+					<p>
+						Lorem, ipsum dolor aaaaaaamaaaaaet consectetur adipisicing elit.
+						Nulla dignissimos nostrum minima
+					</p>
+				</div>
+			</div>
+		);
+}
