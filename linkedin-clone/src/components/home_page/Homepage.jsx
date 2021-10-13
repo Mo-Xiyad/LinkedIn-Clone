904414990d0e7490413e90a039f2f7d5d37f1893
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-13 20:30:25 +0200

    some update

--- linkedin-clone/src/components/home_page/Homepage.jsx
+++ linkedin-clone/src/components/home_page/Homepage.jsx
@@ -1,15 +1,19 @@
-import React from "react";
-import { fetchData } from "../../assats/js";
-import Feed from "./Feed";
-import Sidebar from "./Sidebar";
-import Widget from "./Widget";
+import React from 'react';
+import { fetchData } from '../../assats/js';
+import Feed from './Feed';
+import Sidebar from './Sidebar';
+import Widget from './Widget';
+import Hidden from '@mui/material/Hidden';
+import { Container } from 'react-bootstrap';
 
 export default function Homepage({ authorized }) {
-  return (
-    <div className="homepage">
-      <Sidebar />
-      <Feed authorized={authorized} />
-      {/* <Widget /> */}
-    </div>
-  );
+	return (
+		<Container className="homepage">
+			<Sidebar />
+			<Feed authorized={authorized} />
+			<Hidden mdDown>
+				<Widget />
+			</Hidden>
+		</Container>
+	);
 }
