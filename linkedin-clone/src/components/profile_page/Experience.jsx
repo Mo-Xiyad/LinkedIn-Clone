25a5ad1906cc871e4e0716ee4aa7993543ab41f8
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-13 12:54:08 +0200

    update

--- linkedin-clone/src/components/profile_page/Experience.jsx
+++ linkedin-clone/src/components/profile_page/Experience.jsx
@@ -9,8 +9,10 @@ export default function Experience() {
 					className="img-fluid wiges_onlibottom_listimg"
 					src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
 				/>
-				<div className="activity_onlybottom_list_txt">
+				<div className="experience_list_txt">
 					<h5>Lorem, ipsum dolor onsectetur</h5>
+					<p>User Comented</p>
+					<p>User Comented</p>
 					<p>User Comented</p>
 				</div>
 			</div>
