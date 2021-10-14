3aa6b2c68346a47fd37d0feb82a6bcdfb87aa721
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-10-14 13:40:57 +0200

    side user info

--- linkedin-clone/src/components/home_page/Sidebar.jsx
+++ linkedin-clone/src/components/home_page/Sidebar.jsx
@@ -1,69 +1,71 @@
-import { Avatar } from '@material-ui/core';
-import React from 'react';
-import TurnedInIcon from '@mui/icons-material/TurnedIn';
-import AssistantPhotoIcon from '@mui/icons-material/AssistantPhoto';
-import Hidden from '@mui/material/Hidden';
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import TurnedInIcon from "@mui/icons-material/TurnedIn";
+import AssistantPhotoIcon from "@mui/icons-material/AssistantPhoto";
+import Hidden from "@mui/material/Hidden";
+import { Image } from "react-bootstrap";
 
-export default function Sidebar(props) {
-	const recentitem = (text) => (
-		<div className="recentitembox">
-			<span className="recentitem_#">#</span>
-			<p className="recentitem_p">{text}</p>
-		</div>
-	);
+export default function Sidebar({ authorized }) {
+  const recentitem = (text) => (
+    <div className="recentitembox">
+      <span className="recentitem_#">#</span>
+      <p className="recentitem_p">{text}</p>
+    </div>
+  );
 
-	return (
-		<div className="sidebar">
-			<div className="sidebar_main">
-				<div className="sidebar_avatar">
-					<img
-						className="img-fluid"
-						// src={props.img}
-						src="https://png.pngtree.com/background/20210714/original/pngtree-blue-carbon-background-with-sport-style-and-golden-light-picture-image_1200848.jpg"
-					/>
-					<Avatar />
-					<h3>Props.name</h3>
-					<h5>React Developer| Mobile App | Web Page | UXUI </h5>
-				</div>
+  return (
+    <div className="sidebar">
+      <div className="sidebar_main">
+        <div className="sidebar_avatar">
+          <Image
+            className="img-fluid"
+            src="https://ischool.uw.edu/sites/default/files/2017-08/george_header.jpg"
+          />
+          <div className="sidebar-user-profile-img mb-3">
+            <Image class="img-fluid" src={authorized.image} />
+          </div>
+          <h3>{authorized.name}</h3>
+          <h5>{authorized.title}</h5>
+        </div>
 
-				<div className="sidebar_texts">
-					<div className="textclass_1">
-						<h6 className="text">Connections</h6>
-						<h6 className="number">121</h6>
-					</div>
+        <div className="sidebar_texts">
+          <div className="textclass_1">
+            <h6 className="text">Connections</h6>
+            <h6 className="number">121</h6>
+          </div>
 
-					<div className="textclass_1">
-						<h6 className="text">Who viewed you</h6>
-						<h6 className="number">21</h6>
-					</div>
-					<Hidden smDown>
-						<div className="textclass_2">
-							<h6>
-								<AssistantPhotoIcon color="success" />
-								Try Premium for free
-							</h6>
-						</div>
-						<div className="textclass_2">
-							<h6>
-								<TurnedInIcon /> My items
-							</h6>
-						</div>
-					</Hidden>
-				</div>
-			</div>
-			<Hidden smDown>
-				<div className="sidebar_recent">
-					<h5>Recent</h5>
-					<div>
-						{recentitem('Lorem ipsum dolor sit amet ')}
-						{recentitem('Praesentium, placeat? Veritatis, quia!')}
-						{recentitem('laborum temporibus nam hic')}
-						{recentitem('Lorem ipsum dolor sit amet ')}
-						{recentitem('Praesentium, placeat? Veritatis, quia!')}
-						{recentitem('laborum temporibus nam hic')}
-					</div>
-				</div>
-			</Hidden>
-		</div>
-	);
+          <div className="textclass_1">
+            <h6 className="text">Who viewed you</h6>
+            <h6 className="number">21</h6>
+          </div>
+          <Hidden smDown>
+            <div className="textclass_2">
+              <h6>
+                <AssistantPhotoIcon color="success" />
+                Try Premium for free
+              </h6>
+            </div>
+            <div className="textclass_2">
+              <h6>
+                <TurnedInIcon /> My items
+              </h6>
+            </div>
+          </Hidden>
+        </div>
+      </div>
+      <Hidden smDown>
+        <div className="sidebar_recent">
+          <h5>Recent</h5>
+          <div>
+            {recentitem("Lorem ipsum dolor sit amet ")}
+            {recentitem("Praesentium, placeat? Veritatis, quia!")}
+            {recentitem("laborum temporibus nam hic")}
+            {recentitem("Lorem ipsum dolor sit amet ")}
+            {recentitem("Praesentium, placeat? Veritatis, quia!")}
+            {recentitem("laborum temporibus nam hic")}
+          </div>
+        </div>
+      </Hidden>
+    </div>
+  );
 }
