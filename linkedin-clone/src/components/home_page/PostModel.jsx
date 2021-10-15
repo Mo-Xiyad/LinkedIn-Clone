0a905c02fd2f0812b2ac8400b1b50c855755dbf8
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-15 16:39:09 +0200

    pushing

--- linkedin-clone/src/components/home_page/PostModel.jsx
+++ linkedin-clone/src/components/home_page/PostModel.jsx
@@ -1,175 +1,178 @@
-import React, { useState } from "react";
-import { Image, Modal, Button, Form, Container } from "react-bootstrap";
-import PublicRoundedIcon from "@mui/icons-material/PublicRounded";
-import PanoramaIcon from "@mui/icons-material/Panorama";
-import YouTubeIcon from "@mui/icons-material/YouTube";
-import DescriptionIcon from "@mui/icons-material/Description";
-import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
-import EqualizerIcon from "@mui/icons-material/Equalizer";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import ChatIcon from "@mui/icons-material/Chat";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
-import Postinput from "./Postinput";
-import Hidden from "@mui/material/Hidden";
+import React, { useState } from 'react';
+import { Image, Modal, Button, Form, Container } from 'react-bootstrap';
+import PublicRoundedIcon from '@mui/icons-material/PublicRounded';
+import PanoramaIcon from '@mui/icons-material/Panorama';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import DescriptionIcon from '@mui/icons-material/Description';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import EqualizerIcon from '@mui/icons-material/Equalizer';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import ChatIcon from '@mui/icons-material/Chat';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import Postinput from './Postinput';
+import Hidden from '@mui/material/Hidden';
 function ProfileModal({ show, setShow, authorized, getdata }) {
-  const [values, setValues] = useState();
-  const [image, setImage] = useState(null);
+	const [values, setValues] = useState();
+	const [image, setImage] = useState(null);
 
-  const handleSubmit = async () => {
-    try {
-      const response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/posts/",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: values }),
-          headers: {
-            "Content-Type": "application/json",
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTY0OTE0M2E4OTBjYzAwMTVjZjA3ZjQiLCJpYXQiOjE2MzM5ODA3MzksImV4cCI6MTYzNTE5MDMzOX0.KuT_PgG7s0jzEK_t0MA93LSE7cV3svViPrJzVXJeJ-o",
-          },
-        }
-      );
-      if (response.ok) {
-        let data = await response.json();
+	const handleSubmit = async () => {
+		try {
+			const response = await fetch(
+				'https://striveschool-api.herokuapp.com/api/posts/',
+				{
+					method: 'POST',
+					body: JSON.stringify({ text: values }),
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization:
+							'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTY0OTE0M2E4OTBjYzAwMTVjZjA3ZjQiLCJpYXQiOjE2MzM5ODA3MzksImV4cCI6MTYzNTE5MDMzOX0.KuT_PgG7s0jzEK_t0MA93LSE7cV3svViPrJzVXJeJ-o',
+					},
+				},
+			);
+			if (response.ok) {
+				let data = await response.json();
+				if (image) {
+					try {
+						let formdata = new FormData();
+						formdata.append('post', image);
 
-        try {
-          let formdata = new FormData();
-          formdata.append("post", image);
+						const response = await fetch(
+							`https://striveschool-api.herokuapp.com/api/posts/${data._id}`,
+							{
+								method: 'POST',
+								body: formdata,
+								headers: {
+									Authorization:
+										'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTY0OTE0M2E4OTBjYzAwMTVjZjA3ZjQiLCJpYXQiOjE2MzM5ODA3MzksImV4cCI6MTYzNTE5MDMzOX0.KuT_PgG7s0jzEK_t0MA93LSE7cV3svViPrJzVXJeJ-o',
+								},
+							},
+						);
+						if (response) {
+							setShow(false);
+							getdata();
+						}
+					} catch (error) {
+						console.log(error);
+					}
+				}
+				setShow(false);
+				getdata();
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-          const response = await fetch(
-            `https://striveschool-api.herokuapp.com/api/posts/${data._id}`,
-            {
-              method: "POST",
-              body: formdata,
-              headers: {
-                Authorization:
-                  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTY0OTE0M2E4OTBjYzAwMTVjZjA3ZjQiLCJpYXQiOjE2MzM5ODA3MzksImV4cCI6MTYzNTE5MDMzOX0.KuT_PgG7s0jzEK_t0MA93LSE7cV3svViPrJzVXJeJ-o",
-              },
-            }
-          );
-          if (response) {
-            setShow(false);
-            getdata();
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const addHashtag = (e) => {
+		e.preventDefault();
+		let str = '#';
+		// let content = "";
+		// content += str.repeat(1);
+		return str;
+	};
+	return (
+		// <Modal show={show} onHide={() => setShow(false)}>
+		<Modal
+			dialogClassName="posting_model"
+			size="lg"
+			show={show}
+			onHide={() => setShow(false)}
+		>
+			<Modal.Header closeButton>
+				<Modal.Title>Create a post</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<div className="position-relative d-flex mb-3">
+					<div className="post-input-model d-flex">
+						<Image className="img-fluid" src={authorized.image} />
+						<div>
+							<p className="mb-0">
+								<strong>{authorized.name}</strong>
+							</p>
+							<Button>
+								<p className="mb-0">
+									<span>
+										<PublicRoundedIcon />{' '}
+									</span>
+									Anyone{' '}
+								</p>
+							</Button>{' '}
+						</div>
+					</div>
+				</div>
+				<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+					<Form.Control
+						as="textarea"
+						placeholder="What do you want to talk about?"
+						value={values}
+						onChange={(e) => setValues(e.target.value)}
+					/>
+				</Form.Group>
+				<div>
+					<Button
+						className="hastag_posting_btn"
+						onClick={(e) => setValues(...addHashtag(e))}
+					>
+						Add hastag
+					</Button>
+				</div>
+			</Modal.Body>
+			<Modal.Footer className="posting_model_footer_cont">
+				<div className="posting_model_footer_group_btn_1">
+					{/* ================================= */}
+					<div className="position-relative">
+						<label class="file-upload">
+							<input
+								type="file"
+								// onChange={(e) => console.log(e.target.files[0])}
+								onChange={(e) => setImage(e.target.files[0])}
+							/>
+							<PanoramaIcon fontSize="large" style={{ color: '#666666' }} />
+						</label>
+					</div>
 
-  const addHashtag = (e) => {
-    e.preventDefault();
-    let str = "#";
-    // let content = "";
-    // content += str.repeat(1);
-    return str;
-  };
-  return (
-    // <Modal show={show} onHide={() => setShow(false)}>
-    <Modal
-      dialogClassName="posting_model"
-      size="lg"
-      show={show}
-      onHide={() => setShow(false)}
-    >
-      <Modal.Header closeButton>
-        <Modal.Title>Create a post</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div className="position-relative d-flex mb-3">
-          <div className="post-input-model d-flex">
-            <Image className="img-fluid" src={authorized.image} />
-            <div>
-              <p className="mb-0">
-                <strong>{authorized.name}</strong>
-              </p>
-              <Button>
-                <p className="mb-0">
-                  <span>
-                    <PublicRoundedIcon />{" "}
-                  </span>
-                  Anyone{" "}
-                </p>
-              </Button>{" "}
-            </div>
-          </div>
-        </div>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Control
-            as="textarea"
-            placeholder="What do you want to talk about?"
-            value={values}
-            onChange={(e) => setValues(e.target.value)}
-          />
-        </Form.Group>
-        <div>
-          <Button
-            className="hastag_posting_btn"
-            onClick={(e) => setValues(...addHashtag(e))}
-          >
-            Add hastag
-          </Button>
-        </div>
-      </Modal.Body>
-      <Modal.Footer className="posting_model_footer_cont">
-        <div className="posting_model_footer_group_btn_1">
-          {/* ================================= */}
-          <div className="position-relative">
-            <label class="file-upload">
-              <input
-                type="file"
-                // onChange={(e) => console.log(e.target.files[0])}
-                onChange={(e) => setImage(e.target.files[0])}
-              />
-              <PanoramaIcon fontSize="large" style={{ color: "#666666" }} />
-            </label>
-          </div>
+					{/* ================================= */}
 
-          {/* ================================= */}
+					<div>
+						<YouTubeIcon fontSize="large" style={{ color: '#666666' }} />
+					</div>
 
-          <div>
-            <YouTubeIcon fontSize="large" style={{ color: "#666666" }} />
-          </div>
+					<div>
+						<DescriptionIcon fontSize="large" style={{ color: '#666666' }} />
+					</div>
 
-          <div>
-            <DescriptionIcon fontSize="large" style={{ color: "#666666" }} />
-          </div>
+					<div className="d-none d-lg-inline">
+						<BusinessCenterIcon fontSize="large" style={{ color: '#666666' }} />
+					</div>
 
-          <div className="d-none d-lg-inline">
-            <BusinessCenterIcon fontSize="large" style={{ color: "#666666" }} />
-          </div>
+					<div className="d-none d-lg-inline">
+						<Brightness7Icon fontSize="large" style={{ color: '#666666' }} />
+					</div>
 
-          <div className="d-none d-lg-inline">
-            <Brightness7Icon fontSize="large" style={{ color: "#666666" }} />
-          </div>
+					<div className="d-none d-lg-inline">
+						<EqualizerIcon fontSize="large" style={{ color: '#666666' }} />
+					</div>
 
-          <div className="d-none d-lg-inline">
-            <EqualizerIcon fontSize="large" style={{ color: "#666666" }} />
-          </div>
+					<div>
+						<MoreHorizIcon fontSize="large" style={{ color: '#666666' }} />
+					</div>
+				</div>
 
-          <div>
-            <MoreHorizIcon fontSize="large" style={{ color: "#666666" }} />
-          </div>
-        </div>
+				<div className=" d-none d-sm-inline posting_model_footer_group_btn_2">
+					<ChatIcon style={{ color: '#666666' }} />
+					Anyone
+				</div>
 
-        <div className=" d-none d-sm-inline posting_model_footer_group_btn_2">
-          <ChatIcon style={{ color: "#666666" }} />
-          Anyone
-        </div>
-
-        <Button
-          className="posting_model_post_btn"
-          variant="primary"
-          onClick={() => handleSubmit()}
-        >
-          Post
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+				<Button
+					className="posting_model_post_btn"
+					variant="primary"
+					onClick={() => handleSubmit()}
+				>
+					Post
+				</Button>
+			</Modal.Footer>
+		</Modal>
+	);
 }
 
 export default ProfileModal;
--- linkedin-clone/src/components/profile_page/AddProfileImage.jsx
+++ linkedin-clone/src/components/profile_page/AddProfileImage.jsx
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 import { Image, Modal, Button, Form } from "react-bootstrap";
 import MultiMenus from "../../assats/css/profile_page css/profile-picUpdate-Model.css";
 import PanoramaIcon from "@mui/icons-material/Panorama";
-
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import AddAPhotoOutlinedIcon from '@mui/icons-material/AddAPhotoOutlined';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import PhotoSizeSelectActualOutlinedIcon from '@mui/icons-material/PhotoSizeSelectActualOutlined';
 function AddProfileImage({ picUpdate, setPicUpdate, authorized, fetchUser }) {
   const [picture, setPicture] = useState(null);
 
@@ -33,33 +36,36 @@ function AddProfileImage({ picUpdate, setPicUpdate, authorized, fetchUser }) {
   };
 
   return (
-    <Modal show={picUpdate} onHide={() => setPicUpdate(false)}>
-      <Modal.Header className="img-upload-box" closeButton>
-        <Modal.Title>Profile Picture</Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="profile-pic-update-body">
-        <div className="img-container d-flex justify-content-center">
-          <Image className="img-fluid" src={authorized.image} />
-        </div>
+		<Modal show={true} onHide={() => setPicUpdate(false)}>
+			{/* <Modal show={picUpdate} onHide={() => setPicUpdate(false)}> */}
+			<Modal.Header className="img-upload-box" closeButton>
+				<Modal.Title>Profile Picture</Modal.Title>
+			</Modal.Header>
+			<Modal.Body className="profile-pic-update-body">
+				<div className="img-container d-flex justify-content-center">
+					<Image className="img-fluid" src={authorized.image} />
+				</div>
 
-        <label class="custom-file-upload">
-          <input type="file" onChange={(e) => setPicture(e.target.files[0])} />
-          <PanoramaIcon /> upload
-        </label>
-
-        {/* <Form.Control
+				{/* <Form.Control
           type="file"
           placeholder="update picture"
           onChange={(e) => setPicture(e.target.files[0])}
         /> */}
-      </Modal.Body>
-      <Modal.Footer className="img-upload-box-footer">
-        <Button variant="primary" onClick={() => uploadPicture()}>
-          <h5> Save </h5>
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+			</Modal.Body>
+			<Modal.Footer className="img-upload-box-footer">
+				<label class="custom-file-upload">
+					<input type="file" onChange={(e) => setPicture(e.target.files[0])} />
+					<AddAPhotoOutlinedIcon />
+					<p>Add Photo</p>
+        </label>
+        
+				<Button className="final_btn__" onClick={() => uploadPicture()}>
+					<DeleteForeverIcon />
+					<h5> Delete </h5>
+				</Button>
+			</Modal.Footer>
+		</Modal>
+	);
 }
 
 export default AddProfileImage;
