4f19ddb77d40ea06c093aed1c1ef4b8e5b7946bf
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-15 06:19:21 +0200

    buttons done

--- linkedin-clone/src/components/profile_page/Edu_list.jsx
+++ linkedin-clone/src/components/profile_page/Edu_list.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
-
+import ReorderIcon from '@mui/icons-material/Reorder';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 export default function Edu_list() {
 	return (
 		<>
 			<div className="edu_list_list">
-				<img
-					className="img-fluid wiges_onlibottom_listimg"
-					src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
-				/>
+				<div className="edu_list_sublist">
+					<img
+						className="img-fluid wiges_onlibottom_listimg"
+						src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
+					/>
 
-				<div className="edu_list_list_txt">
-					<h5>Uni name</h5>
-					<p>User Comented</p>
-					<p>User Comented</p>
+					<div className="edu_list_list_txt">
+						<h5>Uni name</h5>
+						<p>User Comented</p>
+						<p>User Comented</p>
+					</div>
+				</div>
+				<div className="btns_vertical_edit">
+					<EditOutlinedIcon fontSize="large" style={{ color: '#a1a09e' }} />
+					<ReorderIcon fontSize="large" style={{ color: '#a1a09e' }} />
 				</div>
 			</div>
 		</>
