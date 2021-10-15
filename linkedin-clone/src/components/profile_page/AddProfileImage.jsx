25eaf459639001bcf0f5269ad0431c7779acca30
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-15 16:49:05 +0200

    final

--- linkedin-clone/src/components/profile_page/AddProfileImage.jsx
+++ linkedin-clone/src/components/profile_page/AddProfileImage.jsx
@@ -1,42 +1,42 @@
-import React, { useState } from "react";
-import { Image, Modal, Button, Form } from "react-bootstrap";
-import MultiMenus from "../../assats/css/profile_page css/profile-picUpdate-Model.css";
-import PanoramaIcon from "@mui/icons-material/Panorama";
+import React, { useState } from 'react';
+import { Image, Modal, Button, Form } from 'react-bootstrap';
+import MultiMenus from '../../assats/css/profile_page css/profile-picUpdate-Model.css';
+import PanoramaIcon from '@mui/icons-material/Panorama';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import AddAPhotoOutlinedIcon from '@mui/icons-material/AddAPhotoOutlined';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import PhotoSizeSelectActualOutlinedIcon from '@mui/icons-material/PhotoSizeSelectActualOutlined';
 function AddProfileImage({ picUpdate, setPicUpdate, authorized, fetchUser }) {
-  const [picture, setPicture] = useState(null);
+	const [picture, setPicture] = useState(null);
 
-  const uploadPicture = async () => {
-    try {
-      let formdata = new FormData();
-      formdata.append("profile", picture);
+	const uploadPicture = async () => {
+		try {
+			let formdata = new FormData();
+			formdata.append('profile', picture);
 
-      const response = await fetch(
-        `https://striveschool-api.herokuapp.com/api/profile/${authorized._id}/picture`,
-        {
-          method: "POST",
-          body: formdata,
-          headers: {
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTY0OTE0M2E4OTBjYzAwMTVjZjA3ZjQiLCJpYXQiOjE2MzM5ODA3MzksImV4cCI6MTYzNTE5MDMzOX0.KuT_PgG7s0jzEK_t0MA93LSE7cV3svViPrJzVXJeJ-o",
-          },
-        }
-      );
-      if (response.ok) {
-        setPicUpdate(false);
-        fetchUser();
-        console.log(response);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+			const response = await fetch(
+				`https://striveschool-api.herokuapp.com/api/profile/${authorized._id}/picture`,
+				{
+					method: 'POST',
+					body: formdata,
+					headers: {
+						Authorization:
+							'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTY0OTE0M2E4OTBjYzAwMTVjZjA3ZjQiLCJpYXQiOjE2MzM5ODA3MzksImV4cCI6MTYzNTE5MDMzOX0.KuT_PgG7s0jzEK_t0MA93LSE7cV3svViPrJzVXJeJ-o',
+					},
+				},
+			);
+			if (response.ok) {
+				setPicUpdate(false);
+				fetchUser();
+				console.log(response);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return (
-		<Modal show={true} onHide={() => setPicUpdate(false)}>
+	return (
+		<Modal show={picUpdate} onHide={() => setPicUpdate(false)}>
 			{/* <Modal show={picUpdate} onHide={() => setPicUpdate(false)}> */}
 			<Modal.Header className="img-upload-box" closeButton>
 				<Modal.Title>Profile Picture</Modal.Title>
@@ -54,14 +54,24 @@ function AddProfileImage({ picUpdate, setPicUpdate, authorized, fetchUser }) {
 			</Modal.Body>
 			<Modal.Footer className="img-upload-box-footer">
 				<label class="custom-file-upload">
+					<EditOutlinedIcon fontSize="large" />
+					<p>Edit</p>
+				</label>
+
+				<label class="custom-file-upload">
 					<input type="file" onChange={(e) => setPicture(e.target.files[0])} />
-					<AddAPhotoOutlinedIcon />
+					<AddAPhotoOutlinedIcon fontSize="large" />
 					<p>Add Photo</p>
-        </label>
-        
+				</label>
+
+				<label class="custom-file-upload">
+					<PhotoSizeSelectActualOutlinedIcon fontSize="large" />
+					<p>Frames</p>
+				</label>
+
 				<Button className="final_btn__" onClick={() => uploadPicture()}>
-					<DeleteForeverIcon />
-					<h5> Delete </h5>
+					<DeleteForeverIcon fontSize="large" />
+					<p> Delete </p>
 				</Button>
 			</Modal.Footer>
 		</Modal>
